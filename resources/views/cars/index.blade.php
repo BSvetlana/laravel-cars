f1dddb798d16e15c0e14dd886d036365bd5be17b
BSvetlana <svetlanabisacki@gmail.com>
2018-04-05 19:44:32 +0200

    seventh commit

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,37 +1,12 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layouts.master');
 
-    <title>@yield('title')</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-</head>
-<body>
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
-                <a href="{{ route('register') }}">Register</a>
-            @endauth
-        </div>
-    @endif
-
-    @foreach($cars as $car)
-
-
-        <a href="/cars/{{$car->id}}">{{$car->title}}</a>
-
-    @endforeach
-
+@section('container')
+</div><!--/.container-->
+<div class="container">
+    <div class="jumbotron">
+        @foreach($cars as $car)
+            <a href="/cars/{{$car->id}}" class="list-group-item">{{$car->title}}</a>
+        @endforeach
+    </div>
 </div>
-</body>
-</html>
+@endsection
--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -1,9 +1,12 @@
+@extends ('layouts.master');
 
-    <div class="card">
-        <div class="card-body">
-            <p>Title: {{ $car->title }}</p>
-            <p>Producer: {{ $car->producer }}<p>
-            <p>Number of doors: {{ $car->number_of_doors }}</p>
-        </div>
+@section('content')
+</div><!--/span-->
+<div class="container">
+    <div class="jumbotron">
+        <h3>{{ $car->title }}</h3>
+        <p class="lead">Produce: {{ $car->producer }}</p>
+        <p class="lead">Number of doors: {{ $car->number_of_doors }}</p>
     </div>
-
+</div>
+@endsection
