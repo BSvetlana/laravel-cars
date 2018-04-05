99d82a2b047a1d2b91f61cfd437b4c83c2eec31a
BSvetlana <svetlanabisacki@gmail.com>
2018-04-05 15:49:25 +0200

    five commit

---
+++ resources/views/cars/index.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Cars</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            @endauth
+        </div>
+    @endif
+        @foreach($cars as $car)
+    <div class="content">
+        <div class="title m-b-md">
+
+                <p>Title: {{ $car->title }}</p>
+                <p>Producer: {{ $car->producer }}<p>
+                <p>Number of doors: {{ $car->number_of_doors }}</p>
+
+        </div>
+
+    </div>
+        @endforeach
+</div>
+</body>
+</html>
